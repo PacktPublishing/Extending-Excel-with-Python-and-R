f10e989d8ba1a5b391b4783d667ae7c74ab956de
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2023-07-16 22:21:27 -0400

    some chapter 5 scripts

---
+++ chapter5/install_ch5_packages.R
@@ -0,0 +1,11 @@
+# styledtabls
+install.packages("devtools")
+
+# Install development version from GitHub
+devtools::install_github('R-package/styledTables', build_vignettes = TRUE)
+
+# excelR
+install.packages('excelR')
+
+# basictabler
+install.packages('basictabler')
