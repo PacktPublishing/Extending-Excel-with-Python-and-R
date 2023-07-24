8e1ca04673deafca858997e376a41e7b4bb29a5c
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2023-07-23 22:21:15 -0400

    Update using_basictabler.R

--- chapter5/using_basictabler.R
+++ chapter5/using_basictabler.R
@@ -12,13 +12,17 @@ table_plain <- qhtbl(data, theme = "largeplain")
 table_plain
 
 # Create a basictabler object
-table <- qhtbl(data, 
+table <- qhtbl(data,
   theme = "largeplain",
-  tableStyle=list("border-color"="maroon"),
-  headingStyle=list("color"="cornsilk", "background-color"="maroon", 
-                    "font-style"="italic", "border-color"="maroon"), 
-  cellStyle=list("color"="maroon", "background-color"="cornsilk", 
-                 "border-color"="maroon")
+  tableStyle = list("border-color" = "maroon"),
+  headingStyle = list(
+    "color" = "cornsilk", "background-color" = "maroon",
+    "font-style" = "italic", "border-color" = "maroon"
+  ),
+  cellStyle = list(
+    "color" = "maroon", "background-color" = "cornsilk",
+    "border-color" = "maroon"
+  )
 )
 
 # Render the table to HTML
@@ -30,16 +34,38 @@ tn <- tidy_normal(.n = 10)
 
 tbl <- BasicTable$new()
 # formatting values (explained in the introduction vignette)
-columnFormats=list(
-  NULL, 
-  NULL, 
+columnFormats <- list(
+  NULL,
+  NULL,
   "%.4f",
   "%.4f",
   "%.4f",
   "%.4f",
   "%.4f"
 )
-tbl$addData(tn, firstColumnAsRowHeaders=TRUE,
-            explicitColumnHeaders=c("Simulation","x","y","dx","dy","p","q"),
-            columnFormats = columnFormats)
+tbl$addData(tn,
+  firstColumnAsRowHeaders = TRUE,
+  explicitColumnHeaders = c("Simulation", "x", "y", "dx", "dy", "p", "q"),
+  columnFormats = columnFormats
+)
+tbl$renderTable()
+
+# Add some conditional formatting
+cells <- tbl$getCells(rowNumbers = 2:11, columnNumbers = 3:7, matchMode = "combinations")
+
+tbl$mapStyling(
+  cells = cells, styleProperty = "background-color", valueType = "color",
+  mapType = "logic",
+  mappings = list(
+    "v<=-3", "red",
+    "-3<v<=-2", "orange",
+    "-2<v<=-1", "pink",
+    "-1<v<= 0", "white",
+    "0<v<=1", "white",
+    "1<v<=2", "lightgreen",
+    "2<v<=3", "lightblue",
+    "3<v", "green"
+  )
+)
+
 tbl$renderTable()
