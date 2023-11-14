005bcc888a8d2c549a5f12ab6aaa6733cbba4eb5
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2023-11-13 22:26:25 -0500

    updates for chapter 12

--- Chapter12/plumber_api.R
+++ Chapter12/plumber_api.R
@@ -1,28 +1,8 @@
-# plumber and Excel
-
 #* Plot out data from a random normal distribution
-#* @param .mean The mean of the standard normal deviation
+#* @param .mean The mean of the standard normal distribution
 #* @get /plot
 #* @serializer png
 function(.mean) {
   mu <- as.numeric(.mean)
   hist(rnorm(n = 1000, mean = mu, sd = 1))
-}
-
-#* Plot out data from the iris dataset
-#* @param spec If provided, filter the data to only this species (e.g. 'setosa')
-#* @get /irisplot
-#* @serializer png
-function(spec){
-  myData <- iris
-  title <- "All Species"
-  
-  # Filter if the species was specified
-  if (!missing(spec)){
-    title <- paste0("Only the '", spec, "' Species")
-    myData <- subset(iris, Species == spec)
-  }
-  
-  plot(myData$Sepal.Length, myData$Petal.Length,
-       main=title, xlab="Sepal Length", ylab="Petal Length")
 }
